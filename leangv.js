c6e0c95ad8d625dd7016b746862a275ee513f95d
Jonathan Kirst <jc@blueigloo.us>
2013-09-01 21:16:30 -0700

    Adding delete forever.

--- leangv.js
+++ leangv.js
@@ -1,6 +1,9 @@
 var getMessagesUrl = "https://www.google.com/voice/inbox/recent/";
+var getTrashMessagesUrl = "https://www.google.com/voice/inbox/recent/trash";
 var deleteMessagesUrl = "https://www.google.com/voice/inbox/deleteMessages/"
+var deleteMessagesForeverUrl = "https://www.google.com/voice/inbox/deleteForeverMessages/"
 var rnr_se = ""
+var mode = ""
 
 var messages = ""
 
@@ -20,24 +23,42 @@ var addToMessages = function(response){
 }
 
 var deleteMessages = function(){
-  var data = messages + "_rnr_se="+rnr_se+"&trash=1"
-  $.ajax(deleteMessagesUrl,
+  var data = messages + "_rnr_se="+rnr_se
+  var url = ""
+  if(mode == "forever"){
+    url = deleteMessagesForeverUrl;
+  } else {
+    data = data + "&trash=1"
+    url = deleteMessagesUrl;
+  }
+  $.ajax(url,
     {
       type: "POST",
       data: data,
-      complete: getMessages,
+      complete: function(x, s){ if(s != "error"){ getMessages(); } },
       error: function(response){ console.log(response) }
     }
   )
 }
 
-var getMessages = function(rnrse){
-  rnr_se = rnrse;
+var getMessages = function(){
   messages = "";
-  $.ajax(getMessagesUrl, 
+  var url = "";
+  if(mode == "forever"){
+    url = getTrashMessagesUrl;
+  } else {
+    url = getMessagesUrl;
+  }
+  $.ajax(url, 
     {
       success: addToMessages,
       error: function(response){ console.log(response) }
     }
   )
+}
+
+var start = function(r, m){
+  rnr_se = r;
+  mode = m
+  getMessages();
 }
